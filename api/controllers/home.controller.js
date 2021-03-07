579f97cf2a95d65e39f5a4780a89983ded1e4f2e
tunahanertekin <tertekin@etu.edu.tr>
2021-03-07 03:04:29 +0300

    sample home model is added

--- api/controllers/home.controller.js
+++ api/controllers/home.controller.js
@@ -1,49 +1,39 @@
 const express = require('express')
 
+const Home = require('../sample/models/home.model')
+
 const homeRouter = express.Router()
+
+const home = new Home()
 
 homeRouter.get('/', (req, res) => {
     const url = req.baseUrl.substring();
     const user_id = url.substring(11, url.lastIndexOf('homes')-1)
-    res.status(200).json({
-        description: "get all homes of a user",
-        user_id: user_id
-    })
+    res.status(200).json(home.getHomes(req.body, req.params, user_id))
+})
+
+homeRouter.get('/:id', (req, res) => {
+    const url = req.baseUrl.substring();
+    const user_id = url.substring(11, url.lastIndexOf('homes')-1)
+    res.status(200).json(home.getHome(req.body, req.params, user_id))
 })
 
 homeRouter.post('/', (req, res) => {
     const url = req.baseUrl.substring();
     const user_id = url.substring(11, url.lastIndexOf('homes')-1)
-    const newHome = req.body;
-    res.status(200).json({
-        description: "add a new home",
-        user_id: user_id,
-        home: newHome
-    });
+    res.status(200).json(home.addHome(req.body, req.params, user_id))
 })
 
 homeRouter.delete('/:id', (req, res) => {
     const url = req.baseUrl.substring();
     const user_id = url.substring(11, url.lastIndexOf('homes')-1)
-    const { id } = req.params;
-    res.status(200).json({
-        description: "delete a home",
-        user_id: user_id,
-        home_id: id
-    })
+    res.status(200).json(home.deleteHome(req.body, req.params, user_id))
 })
 
 homeRouter.patch('/:id', (req, res) => {
     const url = req.baseUrl.substring();
     const user_id = url.substring(11, url.lastIndexOf('homes')-1)
-    const { id } = req.params;
-    const home = req.body;
-    res.status(200).json({
-        description: "update a home",
-        user_id: user_id,
-        home_id: id,
-        home: home
-    });
+    res.status(200).json(home.updateHome(req.body, req.params, user_id))
 })
 
 module.exports = homeRouter;
