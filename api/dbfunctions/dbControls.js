c0596caed96a7cfc11f6240369373a2514e44c30
Teoman Kiraz <t.kiraz@outlook.com>
2021-03-22 12:56:36 +0300

    overall db improvements

--- api/dbfunctions/dbControls.js
+++ api/dbfunctions/dbControls.js
@@ -21,93 +21,89 @@ const pg = require('knex')({
 
 
   function schemaCreation() {
-    knex.schema.createTable('users', table => {
-        table.increments('userid')
-        table.boolean('is_guest')
-        table.string('firstname')
-        table.string('surname')
-        table.string('email')
-        table.string('phone')
-        table.uuid('userid').primary()
-    })
-
-    knex.schema.createTable('home', table => {
-        table.increments('home_id')
-        table.foreign('home_owner').references('users.userid')
-        table.string('home_name')
-        table.string('country')
-        table.string('area')
-        table.string('city')
-        table.string('neighbourhood')
-        table.float('latitude')
-        table.float('longitude')
-        table.uuid('home_id').primary()
-    })
-
-    knex.schema.createTable('request', table => {
-        table.foreign('claimer').references('users.userid')
-        table.foreign('house').references('home.home_id')
-        table.string('description')
-        table.string('results')
-        table.uuid(['claimer', 'house']).primary()
-    })
-
-    knex.schema.createTable('events', table => {
-        table.increments('event_id')
-        table.foreign('event_starter').references('users.userid')
-        table.date('event_start')
-        table.date('event_end')
-        table.string('title')
-        table.string('description')
-        table.uuid('event_id').primary()
-    })
-
-    knex.schema.createTable('attend', table => {
-        table.foreign('attendee').references('users.userid')
-        table.foreign('the_event').references('events.event_id')
-        table.uuid(['attendee', 'the_event']).primary()
-    })
-
-
-    knex.schema.createTable('announcement', table => {
-        table.increments('announcement_id')
-        table.foreign('event_starter').references('users.userid')
-        table.binary('image', [2048])
-        table.string('phone')
-        table.string('title')
-        table.string('description')
-        table.inherits('events')
-    })
-
-    knex.schema.createTable('donations', table => {
-        table.inherits('events')
-    })
-
-    knex.schema.createTable('meeting', table => {
-        table.boolean('is_emergency')
-        table.string('country')
-        table.string('area')
-        table.string('city')
-        table.string('neighbourhood')
-        table.float('latitude')
-        table.float('longitude')
-        table.inherits('donation')
-        table.inherits('events')
-    })
-
-    knex.schema.createTable('money', table => {
-        table.string('currency')
-        table.integer('amount')
-        table.inherits('donations')
-    })
-
-    knex.schema.createTable('supply', table => {
-        table.string('country')
-        table.string('area')
-        table.string('city')
-        table.string('neighbourhood')
-        table.float('latitude')
-        table.float('longitude')
-        table.inherits('donation')
-    })
+    return knex.schema.createTable('users', table => {
+      table.increments('userid').primary()
+      table.boolean('is_guest').notNullable()
+      table.string('firstname').notNullable()
+      table.string('surname').notNullable()
+      table.string('email')
+      table.string('phone')
+  })
+  
+  
+  .createTable('home', table => {
+      table.increments('home_id').primary()
+      table.integer('home_owner').references('userid').inTable('users')
+      table.string('home_name')
+      table.string('country')
+      table.string('area')
+      table.string('city')
+      table.string('neighbourhood')
+      table.float('latitude')
+      table.float('longitude')
+  })
+  
+  .createTable('request', table => {
+      table.integer('claimer').references('userid').inTable('users')
+      table.integer('house').references('home_id').inTable('home')
+      table.string('description')
+      table.string('results')
+      table.primary(['claimer', 'house'])
+  })
+  
+  .createTable('events', table => {
+      table.increments('event_id').primary()
+      table.integer('event_starter').references('userid').inTable('users')
+      table.date('event_start')
+      table.date('event_end')
+      table.string('title')
+      table.string('description')
+  })
+  
+  .createTable('attend', table => {
+      table.integer('attendee').references('userid').inTable('users')
+      table.integer('the_event').references('event_id').inTable('events')
+      table.primary(['attendee', 'the_event'])
+  })
+  
+  .createTable('announcement', table => {
+      table.increments('announcement_id').primary()
+      table.integer('ann_starter').references('userid').inTable('users')
+      table.binary('image', [2048])
+      table.string('phone')
+      table.string('title')
+      table.string('description')
+  })
+  
+  .createTable('donations', table => {
+      table.inherits('events')
+  })
+  
+  .createTable('meeting', table => {
+      table.boolean('is_emergency')
+      table.string('country')
+      table.string('area')
+      table.string('city')
+      table.string('neighbourhood')
+      table.float('latitude')
+      table.float('longitude')
+      table.inherits('events')
+  })
+  
+  .createTable('money', table => {
+      table.string('currency')
+      table.integer('amount')
+      table.inherits('donations')
+  })
+  .createTable('supply', table => {
+      table.string('country')
+      table.string('area')
+      table.string('city')
+      table.string('neighbourhood')
+      table.float('latitude')
+      table.float('longitude')
+      table.inherits('donations')
+  })
   }
+
+  module.exports = knex;
