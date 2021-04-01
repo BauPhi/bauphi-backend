b323623715b82c6e58a63cc4b25753758217efeb
tunahanertekin <tertekin@etu.edu.tr>
2021-04-01 21:39:19 +0300

    deployment to heroku

--- api/dbfunctions/knexfile.js
+++ api/dbfunctions/knexfile.js
@@ -1,5 +1,3 @@
-// Update with your config settings.
-
 module.exports = {
 
   development: {
@@ -36,18 +34,17 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
-      database: 'my_db',
-      user:     'postgres',
-      password: '123456p'
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './knex/migrations',
     }
   }
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./api/server')
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(5000, () => {
     console.log("***Server is up on http://localhost:" + PORT + "***");
