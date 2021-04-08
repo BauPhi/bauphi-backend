b14b3506824affe6948f1b9f29a409287f00f880
tunahanertekin <tertekin@etu.edu.tr>
2021-04-08 20:16:58 +0300

    seed data is updated for password hashing

--- api/dbfunctions/knex/seeds/01_users.js
+++ api/dbfunctions/knex/seeds/01_users.js
@@ -4,12 +4,14 @@ exports.seed = function(knex) {
   return knex('users').del()
     .then(function () {
       // Inserts seed entries
+      const sha256 = require('js-sha256');
+
       return knex('users').insert([
-        {user_id: 1, name: "Tunahan", surname: 'Ertekin', email: "tuna@gmail.com", phone: "+905056667788", password: "123"},
-        {user_id: 2, name: "Gözde", surname: 'Önal', email: "gozde@gmail.com", phone: "+905056667788", password: "123"},
-        {user_id: 3, name: "Nilay", surname: 'Yorgancılar', email: "nilay@gmail.com", phone: "+905056667788", password: "123"},
-        {user_id: 4, name: "Öykü", surname: 'Şüyün', email: "oyku@gmail.com", phone: "+905056667788", password: "123"},
-        {user_id: 5, name: "Teoman", surname: 'Kiraz', email: "teoman@gmail.com", phone: "+905056667788", password: "123"}
+        {user_id: 1, name: "Tunahan", surname: 'Ertekin', email: "tuna@gmail.com", phone: "+905056667788", password: sha256("123")},
+        {user_id: 2, name: "Gözde", surname: 'Önal', email: "gozde@gmail.com", phone: "+905056667788", password: sha256("123")},
+        {user_id: 3, name: "Nilay", surname: 'Yorgancılar', email: "nilay@gmail.com", phone: "+905056667788", password: sha256("123")},
+        {user_id: 4, name: "Öykü", surname: 'Şüyün', email: "oyku@gmail.com", phone: "+905056667788", password: sha256("123")},
+        {user_id: 5, name: "Teoman", surname: 'Kiraz', email: "teoman@gmail.com", phone: "+905056667788", password: sha256("123")}
       ]);
     });
 };
